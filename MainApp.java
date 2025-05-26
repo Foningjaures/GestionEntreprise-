e0921523b1c558f92563f0392b488de86dcf2080
Foningjaures <foningjaures20@gmail.com>
2025-05-26 23:58:17 +0100

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,9 @@
+
+package com.eventmanager;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("Lancement du système de gestion d'événements...");
+        // À compléter avec lancement JavaFX
+    }
+}
